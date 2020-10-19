e3c7ca4e207fa30dbd61d86994c33d75ec776488
David Sterry <sterry.david@gmail.com>
2020-10-19 15:53:55 -0700

    add code to reverse letter order
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,17 @@ let sentence = prompt("enter a sentence");
 console.log(sentence);
 
 function caps(x){
-    first = x.charAt(0).toUpperCase();
-    last = x.charAt(x.length-1).toUpperCase();
+  let first = x.charAt(0).toUpperCase();
+  let last = x.charAt(x.length-1).toUpperCase();
   return first + last;
 }
 
-alert(caps(sentence));
+let results = caps(sentence);
+
+function reverse(x){
+   let first = x.charAt(0);
+  let last = x.charAt(1);
+  return last + first;
+}
+
+alert(reverse(results));
